7288ad53b9340ad61371d07b45aa9d595a2ec9e3
Danila Migalin <me@miga.me.uk>
2026-03-29 00:49:16 +0200

    predicates seem to be working

--- src/predicate.rs
+++ src/predicate.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use crate::error::{self, AppError};
+use foundationdb::tuple::TuplePack;
 use sexpression::Expression;
 use snafu::{ResultExt, whatever};
 
@@ -48,7 +49,7 @@ impl<'a> Predicate<'a> {
         }
     }
 
-    fn get_referred_fields(&self, value: &'a rmpv::Value) -> HashMap<&str, &rmpv::Value> {
+    fn get_referred_fields(&self, value: &'a rmpv::Value) -> HashMap<&'a str, Expression<'a>> {
         let mut r = HashMap::new();
         'NEXT_FIELD: for path in &self.referred_fields {
             // path looks like .foo.bar.baz, split it by ".", skip 1st part
@@ -62,6 +63,23 @@ impl<'a> Predicate<'a> {
                     continue 'NEXT_FIELD;
                 }
             }
+            // convert value we got into Expression assuming it is scalar value
+            let tail = match tail {
+                rmpv::Value::Boolean(b) => Expression::Bool(*b),
+                rmpv::Value::Integer(n) => Expression::Number(n.as_f64().unwrap()),
+                rmpv::Value::F32(f) => Expression::Number(*f as f64),
+                rmpv::Value::F64(f) => Expression::Number(*f),
+                rmpv::Value::String(s) => match s.as_str() {
+                    Some(s) => Expression::Str(s),
+                    None => continue 'NEXT_FIELD,
+                },
+                rmpv::Value::Binary(_) => {
+                    continue 'NEXT_FIELD; // TODO: support this? 
+                }
+                _ => {
+                    continue 'NEXT_FIELD;
+                }
+            };
             r.insert(*path, tail);
         }
         r
@@ -82,13 +100,13 @@ impl<'a> Predicate<'a> {
 }
 
 struct Val<'a> {
-    values: &'a HashMap<&'a str, &'a rmpv::Value>,
+    values: &'a HashMap<&'a str, Expression<'a>>,
     expr: &'a sexpression::Expression<'a>,
 }
 
 impl<'a> Val<'a> {
     fn new(
-        values: &'a HashMap<&str, &rmpv::Value>,
+        values: &'a HashMap<&str, Expression<'a>>,
         expr: Option<&'a sexpression::Expression>,
     ) -> Result<Self, AppError> {
         if let Some(expr) = expr {
@@ -115,65 +133,70 @@ impl<'a> Val<'a> {
         Ok(r)
     }
 
-    fn eval_list(&self, exprs: &[sexpression::Expression]) -> Result<bool, AppError> {
-        if exprs.is_empty() {
+    fn eval_list(&self, list: &[sexpression::Expression]) -> Result<bool, AppError> {
+        if list.is_empty() {
             return Ok(true);
         }
-        match &exprs.get(0) {
+        match &list.get(0) {
             Some(Expression::Symbol(op)) => {
                 let result = match *op {
                     "eq" => {
-                        Val::new(self.values, exprs.get(1))? == Val::new(self.values, exprs.get(2))?
+                        Val::new(self.values, list.get(1))? == Val::new(self.values, list.get(2))?
+                    }
+                    "ne" => {
+                        Val::new(self.values, list.get(1))? != Val::new(self.values, list.get(2))?
                     }
                     "in" => {
                         // (in .foo (1 2 3 4))
-                        if exprs.len() < 3 {
+                        if list.len() != 3 {
                             whatever!("syntax error");
                         }
-                        for v in &exprs[2..] {
-                            if Val::new(self.values, exprs.get(1))?
-                                == Val::new(self.values, Some(v))?
-                            {
-                                return Ok(true);
+                        if let Expression::List(items) = &list[2] {
+                            let rhs = Val::new(self.values, list.get(1))?;
+                            for v in items {
+                                if rhs == Val::new(self.values, Some(v))? {
+                                    return Ok(true);
+                                }
                             }
                         }
                         false
                     }
                     "ge" => {
-                        Val::new(self.values, exprs.get(1))? >= Val::new(self.values, exprs.get(2))?
+                        Val::new(self.values, list.get(1))? >= Val::new(self.values, list.get(2))?
                     }
                     "gt" => {
-                        Val::new(self.values, exprs.get(1))? > Val::new(self.values, exprs.get(2))?
+                        eprintln!("{:?} > {:?}", list.get(1), list.get(2));
+                        Val::new(self.values, list.get(1))? > Val::new(self.values, list.get(2))?
                     }
                     "le" => {
-                        Val::new(self.values, exprs.get(1))? <= Val::new(self.values, exprs.get(2))?
+                        Val::new(self.values, list.get(1))? <= Val::new(self.values, list.get(2))?
                     }
                     "lt" => {
-                        Val::new(self.values, exprs.get(1))? < Val::new(self.values, exprs.get(2))?
+                        Val::new(self.values, list.get(1))? < Val::new(self.values, list.get(2))?
                     }
                     "and" => {
                         // (and (eq .foo "chlos") (eq .bar 137))
-                        if exprs.len() < 3 {
+                        if list.len() < 3 {
                             whatever!("syntax error")
                         }
                         let mut r = true;
-                        for v in &exprs[2..] {
+                        for v in &list[1..] {
                             r = r && Val::new(self.values, Some(v))?.evaluate()?;
                         }
                         r
                     }
                     "or" => {
                         // (or (eq .foo "chlos") (eq .bar 137))
-                        if exprs.len() < 3 {
+                        if list.len() < 3 {
                             whatever!("syntax error")
                         }
                         let mut r = false;
-                        for v in &exprs[2..] {
+                        for v in &list[1..] {
                             r = r || Val::new(self.values, Some(v))?.evaluate()?;
                         }
                         r
                     }
-                    "not" => !Val::new(self.values, exprs.get(1))?.evaluate()?,
+                    "not" => !Val::new(self.values, list.get(1))?.evaluate()?,
                     s => {
                         whatever!("invalid operation '{s}'");
                     }
@@ -195,9 +218,8 @@ impl<'a> PartialEq for Val<'a> {
         // atm we don't support any calculations inside predicates, so we expect that
         // both values will be either literal number or string, or document reference
 
-        // first, dereference values
-        let a = if let Expression::Symbol(path) = self.expr {
-            debug_assert!(path.starts_with("."));
+        // if value is document refence, get its value, otherwise return as is
+        let lhs = if let Expression::Symbol(path) = self.expr {
             if let Some(v) = self.values.get(path) {
                 v
             } else {
@@ -206,8 +228,8 @@ impl<'a> PartialEq for Val<'a> {
         } else {
             &self.expr
         };
-        let b = if let Expression::Symbol(path) = other.expr {
-            if let Some(v) = self.get_from_doc(path) {
+        let rhs = if let Expression::Symbol(path) = other.expr {
+            if let Some(v) = self.values.get(path) {
                 v
             } else {
                 return false;
@@ -216,36 +238,196 @@ impl<'a> PartialEq for Val<'a> {
             other.expr
         };
 
-        match (a, b) {
-            (Expression::Number(a), Expression::Number(b)) => a == b,
-            (Expression::Str(a), Expression::Str(b)) => a == b,
-            (Expression::Bool(a), Expression::Bool(b)) => a == b,
-            _ => false,
-        }
+        lhs == rhs
     }
 }
 
 impl<'a> PartialOrd for Val<'a> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        todo!()
+        // if value is document refence, get its value, otherwise return as is
+        let lhs = if let Expression::Symbol(path) = self.expr {
+            if let Some(v) = self.values.get(path) {
+                v
+            } else {
+                return None;
+            }
+        } else {
+            &self.expr
+        };
+        let rhs = if let Expression::Symbol(path) = other.expr {
+            if let Some(v) = self.values.get(path) {
+                v
+            } else {
+                return None;
+            }
+        } else {
+            other.expr
+        };
+
+        match (lhs, rhs) {
+            (Expression::Number(a), Expression::Number(b)) => a.partial_cmp(b),
+            (Expression::Str(a), Expression::Str(b)) => a.partial_cmp(b),
+            (Expression::Bool(a), Expression::Bool(b)) => a.partial_cmp(b),
+            _ => None,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use serde_json::json;
+    use sexpression::Expression;
 
     use crate::{encoding::json2mp, predicate::Predicate};
 
     #[test]
     fn referred_fields_extraction() {
-        let p = Predicate::from_query(r#"(and (eq .foo "chlos") (eq '.baz.bar' 137))"#).unwrap();
+        let p = Predicate::from_query(r#"(and (eq .foo "chlos") (eq .baz.bar 137))"#).unwrap();
         assert!(p.referred_fields.contains(&".foo"));
         assert!(p.referred_fields.contains(&".baz.bar"));
         let v = json2mp(json!({"foo": 123, "baz": {"bar": "chlos", "bak": true}}));
         let fields = p.get_referred_fields(&v);
-        assert_eq!(fields.get(".foo"), Some(&&rmpv::Value::from(123)));
-        assert_eq!(fields.get(".baz.bar"), Some(&&rmpv::Value::from("chlos")));
+        assert_eq!(fields.get(".foo"), Some(&Expression::Number(123.0)));
+        assert_eq!(fields.get(".baz.bar"), Some(&Expression::Str("chlos")));
         assert!(!fields.contains_key(".baz.bak"));
+    }
+
+    #[test]
+    fn evaluation() {
+        let v = json2mp(json!({"foo": "chlos", "baz": {"bar": 137, "bak": true}}));
+
+        // empty query
+        assert_eq!(
+            Predicate::from_query(r#"()"#).unwrap().execute(&v).unwrap(),
+            true
+        );
+
+        // always true
+        assert_eq!(
+            Predicate::from_query(r#"true"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // always false
+        assert_eq!(
+            Predicate::from_query(r#"false"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            false
+        );
+
+        // simple comparison
+        assert_eq!(
+            Predicate::from_query(r#"(eq .foo "chlos")"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // simple comparison
+        assert_eq!(
+            Predicate::from_query(r#"(ge .baz.bar 137)"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // simple comparison
+        assert_eq!(
+            Predicate::from_query(r#"(le .baz.bar 137)"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // simple comparison
+        assert_eq!(
+            Predicate::from_query(r#"(lt .baz.bar 1000)"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // simple comparison
+        assert_eq!(
+            // Note that currently the upstream version of s-expressions is incorrectly parsing single-digit numbers
+            // https://github.com/eckertliam/s-expression/issues/3
+            // so this test will fail with predicate (gt .baz.bar 0)
+            Predicate::from_query(r#"(gt .baz.bar 0.0)"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+        // not
+        assert_eq!(
+            Predicate::from_query(r#"(not (ge .baz.bar 10))"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            false
+        );
+
+        // not equal
+        assert_eq!(
+            Predicate::from_query(r#"(ne .baz.bar 166)"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // in
+        assert_eq!(
+            Predicate::from_query(r#"(in .foo ("chlos" "CHLOS" "chicken))"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // and
+        assert_eq!(
+            Predicate::from_query(r#"(and (eq .foo "chlos") (eq .baz.bar 137))"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // and, no match
+        assert_eq!(
+            Predicate::from_query(r#"(and (eq .foo "CHLOS") (eq .baz.bar 137))"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            false
+        );
+
+        // or
+        assert_eq!(
+            Predicate::from_query(r#"(or (eq .foo "chlos") (eq .baz.bar 0))"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            true
+        );
+
+        // or, no match
+        assert_eq!(
+            Predicate::from_query(r#"(or (eq .foo "CHLOS") (eq .baz.bar 0))"#)
+                .unwrap()
+                .execute(&v)
+                .unwrap(),
+            false
+        );
     }
 }
